e46a969758a64e15015c83f7e25ca2fb211a523b
Suerine Otieno <suerineogaja@gmail.com>
2026-05-24 17:42:48 +0300

    Feat: fixing pic upload for product

--- backend/server.js
+++ backend/server.js
@@ -102,4 +102,12 @@ const startServer = async () => {
   }
 };
 
+app.get("/api/debug-env", (req, res) => {
+  res.json({
+    cloud_name: !!process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: !!process.env.CLOUDINARY_API_KEY,
+    api_secret: !!process.env.CLOUDINARY_API_SECRET,
+  })
+})
+
 startServer();
